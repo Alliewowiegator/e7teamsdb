4b6bd83677407f1e2e78f08cbc3222203e4a4078
Alliewowiegator <alliegarza310@gmail.com>
2022-08-20 00:37:00 -0500

    Fixes to hydration errors caused by Typography containing FormControl

--- pages/_app.js
+++ pages/_app.js
@@ -1,24 +1,38 @@
 import { ThemeProvider } from '@mui/system'
+import PropTypes from 'prop-types';
 import '../styles/globals.css'
 import Navbar from './navbar'
-
+import { CacheProvider } from '@emotion/react';
 import { createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
+import createEmotionCache from '../utility/createEmotionCache';
 
+const clientSideEmotionCache = createEmotionCache();
 const darkTheme = createTheme({
   palette: {
     mode: 'dark',
   },
 });
 
-function MyApp({ Component, pageProps }) {
+
+const MyApp = (props) => {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
   return (
-    <ThemeProvider theme={darkTheme}>
-      <CssBaseline />
-      <Navbar />
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <CacheProvider value={emotionCache}>
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline />
+        <Navbar />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+
   )
 }
 
-export default MyApp
+export default MyApp;
+
+MyApp.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  emotionCache: PropTypes.object,
+  pageProps: PropTypes.object.isRequired,
+};
