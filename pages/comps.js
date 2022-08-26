667d8e69f29eaf476767df0d57e483377b3e2572
Alliewowiegator <alliegarza310@gmail.com>
2022-08-26 12:03:09 -0500

    Added basic filtering to composition list

--- pages/comps.js
+++ pages/comps.js
@@ -1,175 +1,147 @@
-import React from "react";
+/* eslint-disable react/prop-types */
+import React, { useEffect, useState } from "react";
 import Container from "@mui/material/Container";
 import Grid2 from "@mui/material/Unstable_Grid2";
 import {
-  Card,
-  CardContent,
-  Typography,
-  InputLabel,
-  Select,
-  OutlinedInput,
-  Box,
-  Chip,
-  MenuItem,
-  FormControl,
-  IconButton,
-  CardActions,
-  Button,
-  Paper,
-  Avatar,
-  Divider,
+    Card,
+    CardContent,
+    Typography,
+    InputLabel,
+    Select,
+    OutlinedInput,
+    MenuItem,
+    FormControl,
+    Divider,
 } from "@mui/material";
 
-import RefreshIcon from "@mui/icons-material/Refresh";
 import CompPreview from "../components/CompPreview";
 
 const Comps = ({ compData }) => {
-  return (
-    <Container maxWidth="lg">
-      <Grid2
-        container
-        spacing={3}
-        paddingTop="2rem"
-        columns={{ xs: 4, sm: 8, md: 12 }}
-        alignItems="stretch"
-      >
-        <Grid2 item xs={4} md={12}>
-          <Card>
-            <CardContent>
-              <Typography sx={{ fontSize: 14 }} color="primary" gutterBottom>
-                Team Compositions
-              </Typography>
-              <Typography variant="body1">
-                This will allow you to find team compositions that fit your
-                need. Whether it be based on a unit, content type, or blah blah
-                blah.
-              </Typography>
-            </CardContent>
-          </Card>
-        </Grid2>
-        <Grid2 item md={12}></Grid2>
-        <Grid2 item xs={4} md={12}>
-          <Card>
-            <CardContent>
-              <Typography sx={{ fontSize: 14 }} color="primary" gutterBottom>
-                Team Compositions
-              </Typography>
-              <Grid2
+    const [filteredData, setFilteredData] = useState(compData);
+    const [contentFilter, setContentFilter] = useState("");
+
+    const filteredContent = () => {
+        console.log('called')
+        if (filteredData.length <= 0) {
+            return (
+                <Grid2 item md={12} xs={4}>
+                    <Typography variant="body1">
+                        No Matching Compositions were Found with Your Search Parameters
+                    </Typography>
+                </Grid2>
+            )
+        } else {
+            return (
+                filteredData.map((hero, index) => (
+                    <Grid2 key={index} item md={4} xs={4}>
+                        <CompPreview {...hero}></CompPreview>
+                    </Grid2>
+                ))
+            )
+        }
+    }
+
+    useEffect(() => {
+        if (contentFilter) {
+            setFilteredData(compData.filter(composition => composition.teamInfo.teamType === contentFilter));
+            console.log(filteredData);
+        }
+    }, [contentFilter])
+
+    return (
+        <Container maxWidth="lg">
+            <Grid2
                 container
-                spacing={1}
+                spacing={3}
+                paddingTop="2rem"
                 columns={{ xs: 4, sm: 8, md: 12 }}
                 alignItems="stretch"
-              >
-                <Grid2 item md={3} xs={4}>
-                  <FormControl fullWidth>
-                    <InputLabel id="demo-multiple-chip-label">
-                      Filter By Content
-                    </InputLabel>
-                    <Select
-                      labelId="demo-multiple-chip-label"
-                      id="demo-multiple-chip"
-                      input={
-                        <OutlinedInput
-                          id="select-multiple-chip"
-                          label="Filter By Content"
-                        />
-                      }
-                    >
-                      <MenuItem value={"wyvern"}>Wyvern</MenuItem>
-                      <MenuItem value={"banshee"}>Banshee</MenuItem>
-                      <MenuItem value={"golem"}>Golem</MenuItem>
-                      <MenuItem value={"guildWarDefense"}>
-                        Guild War Defense
-                      </MenuItem>
-                      <MenuItem value={"guildwarOffense"}>
-                        Guild War Offense
-                      </MenuItem>
-                      <MenuItem value={"arenaDefense"}>Arena Defense</MenuItem>
-                      <MenuItem value={"arenaOffense"}>Arena Offense</MenuItem>
-                      <MenuItem value={"rta"}>RTA</MenuItem>
-                      <MenuItem value={"abyss"}>Abyss</MenuItem>
-                      <MenuItem value={"adventure"}>Adventure</MenuItem>
-                      <MenuItem value={"other"}>Other</MenuItem>
-                    </Select>
-                  </FormControl>
+            >
+                <Grid2 item xs={4} md={12}>
+                    <Card>
+                        <CardContent>
+                            <Typography sx={{ fontSize: 14 }} color="primary" gutterBottom>
+                                Team Compositions
+                            </Typography>
+                            <Typography variant="body1">
+                                This will allow you to find team compositions that fit your
+                                need. Whether it be based on a unit, content type, or blah blah
+                                blah.
+                            </Typography>
+                        </CardContent>
+                    </Card>
                 </Grid2>
-                <Grid2 item md={3} xs={4}>
-                  <FormControl fullWidth>
-                    <InputLabel id="demo-multiple-chip-label">
-                      Filter By Hero
-                    </InputLabel>
-                    <Select
-                      labelId="demo-multiple-chip-label"
-                      id="demo-multiple-chip"
-                      multiple
-                      input={
-                        <OutlinedInput
-                          id="select-multiple-chip"
-                          label="Filter By Hero"
-                        />
-                      }
-                      renderValue={(selected) => (
-                        <Box
-                          sx={{ display: "flex", flexWrap: "wrap", gap: 0.5 }}
-                        >
-                          {selected.map((value) => (
-                            <Chip key={value} label={value} />
-                          ))}
-                        </Box>
-                      )}
-                    ></Select>
-                  </FormControl>
+                <Grid2 item md={12}></Grid2>
+                <Grid2 item xs={4} md={12}>
+                    <Card>
+                        <CardContent>
+                            <Typography sx={{ fontSize: 14 }} color="primary" gutterBottom>
+                                Team Compositions
+                            </Typography>
+                            <Grid2
+                                container
+                                spacing={1}
+                                columns={{ xs: 4, sm: 8, md: 12 }}
+                                alignItems="stretch"
+                            >
+                                <Grid2 item md={3} xs={4}>
+                                    <FormControl fullWidth>
+                                        <InputLabel id="demo-multiple-chip-label">
+                                            Filter By Content
+                                        </InputLabel>
+                                        <Select
+                                            onChange={(e) =>
+                                                setContentFilter(e.target.value)
+                                            }
+                                            labelId="demo-multiple-chip-label"
+                                            id="demo-multiple-chip"
+                                            input={
+                                                <OutlinedInput
+                                                    id="select-multiple-chip"
+                                                    label="Filter By Content"
+                                                />
+                                            }
+                                        >
+                                            <MenuItem value={"Wyvern"}>Wyvern</MenuItem>
+                                            <MenuItem value={"Banshee"}>Banshee</MenuItem>
+                                            <MenuItem value={"Golem"}>Golem</MenuItem>
+                                            <MenuItem value={"Guild War Defense"}>
+                                                Guild War Defense
+                                            </MenuItem>
+                                            <MenuItem value={"Guild War Offense"}>
+                                                Guild War Offense
+                                            </MenuItem>
+                                            <MenuItem value={"Arena Defense"}>Arena Defense</MenuItem>
+                                            <MenuItem value={"Arena Offense"}>Arena Offense</MenuItem>
+                                            <MenuItem value={"RTA"}>RTA</MenuItem>
+                                            <MenuItem value={"Abyss"}>Abyss</MenuItem>
+                                            <MenuItem value={"Adventure"}>Adventure</MenuItem>
+                                            <MenuItem value={"Other"}>Other</MenuItem>
+                                        </Select>
+                                    </FormControl>
+                                </Grid2>
+                
+                                <Grid2 item md={12} xs={4}>
+                                    <Divider />
+                                </Grid2>
+                                <Grid2
+                                    container
+                                    spacing={3}
+                                    md={12}
+                                    xs={4}
+                                    paddingTop="2rem"
+                                    columns={{ xs: 4, sm: 8, md: 12 }}
+                                    alignItems="stretch"
+                                >
+                                    { filteredContent() }
+                                </Grid2>
+                            </Grid2>
+                        </CardContent>
+                    </Card>
                 </Grid2>
-                <Grid2 item md={3} xs={4}>
-                  <FormControl fullWidth>
-                    <InputLabel id="demo-multiple-chip-label">
-                      Filter By Something
-                    </InputLabel>
-                    <Select
-                      labelId="demo-multiple-chip-label"
-                      id="demo-multiple-chip"
-                      multiple
-                      input={
-                        <OutlinedInput
-                          id="select-multiple-chip"
-                          label="Filter By Something"
-                        />
-                      }
-                    ></Select>
-                  </FormControl>
-                </Grid2>
-                <Grid2 item md={1} mdOffset={2} xsOffset={1.7}>
-                  <IconButton size="large" aria-label="delete" color="success">
-                    <RefreshIcon />
-                  </IconButton>
-                </Grid2>
-                <Grid2 item md={12} xs={4}>
-                  <Divider />
-                </Grid2>
-                <Grid2 item md={12} xs={4}>
-                  <Grid2
-                    container
-                    spacing={3}
-                    md={12}
-                    xs={4}
-                    paddingTop="2rem"
-                    columns={{ xs: 4, sm: 8, md: 12 }}
-                    alignItems="stretch"
-                  >
-                    {compData.map((hero, index) => (
-                      <Grid2 key={index} item md={4} xs={4}>
-                        <CompPreview {...hero}></CompPreview>
-                      </Grid2>
-                    ))}
-                  </Grid2>
-                </Grid2>
-              </Grid2>
-            </CardContent>
-          </Card>
-        </Grid2>
-      </Grid2>
-    </Container>
-  );
+            </Grid2>
+        </Container>
+    );
 };
 
 Comps.getInitialProps = async () => {
--- pages/navbar.js
+++ pages/navbar.js
@@ -16,23 +16,14 @@ import Link from "next/link";
 
 const Navbar = () => {
   const pages = ["New Composition", "View All Compositions"];
-
   const [anchorElNav, setAnchorElNav] = useState(null);
-  const [anchorElUser, setAnchorElUser] = useState(null);
 
   const handleOpenNavMenu = (event) => {
     setAnchorElNav(event.currentTarget);
   };
-  const handleOpenUserMenu = (event) => {
-    setAnchorElUser(event.currentTarget);
-  };
 
   const handleCloseNavMenu = () => {
     setAnchorElNav(null);
-  };
-
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
   };
 
   return (
